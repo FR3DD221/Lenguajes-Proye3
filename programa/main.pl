0d5a080555960571d149708add6acec3f5331c3f
Fredd Badilla Víquez <freddalessandro2876@gmail.com>
2023-10-24 13:31:42 -0600

    Comentarios a las funciones

--- programa/main.pl
+++ programa/main.pl
@@ -1,27 +1,34 @@
 :- dynamic(persona/5).
 
+%Lista de tareas para validar
 tareas(['requerimientos', 'disenio', 'desarrollo', 'qa', 'fullstack', 'frontend', 'backend', 'administracion']).
 
+%Menu principal
 menu:- writeln('1. Gestion de personas'),
     writeln('9. Salir'), write('Ingrese su opcion deseada: '), read_line_to_string(user_input, Opcion), comprobarOpcionMenuMain(Opcion).
 
+%Funcion auxiliar para comprobar que la opcion ingresada por el usuario el correcta
 comprobarOpcionMenuMain(Opcion):- string_lower(Opcion, "1"), menuPersona, menu.
 comprobarOpcionMenuMain(Opcion):- string_lower(Opcion, "9"), !.
 comprobarOpcionMenuMain(_):- writeln('Opcion invalida. Por favor, ingrese una opcion valida.'), menu.
 
+%Menu para la funcion de gestion de personas
 menuPersona:- writeln('1. Agregar de personas'), writeln('2. Imprimir personas'), writeln('3. Volver'), write('Ingrese su opcion: '), 
     read_line_to_string(user_input, Opcion), comprobarOpcionPersona(Opcion).
 
+%Funcion auxiliar para comprobar que la opcion ingresada por el usuario el correcta
 comprobarOpcionPersona(Opcion):- string_lower(Opcion, "1"), ingresarPersonas, menuPersona.
 comprobarOpcionPersona(Opcion):- string_lower(Opcion, "3").
 comprobarOpcionPersona(_):- writeln('Opcion invalida. Por favor, ingrese una opcion valida.'), menuPersona.
 
+%Funcion que pide los datos al usuario sobre un nuevo empleado
 ingresarPersonas:- write('Ingrese el nombre de su trabajador: '), read_line_to_string(user_input, Nombre), nl,
     write('Ingrese el puesto de su trabajador: '), read_line_to_string(user_input, Puesto), nl,
     write('Ingrese el costo por tarea de su trabajador: '), read_line_to_string(user_input, Costo), nl,
     write('Ingrese el rating de su trabajador: '), read_line_to_string(user_input, Rating), nl,
     agregarTareas(Nombre, Puesto, Costo, Rating, []).
 
+%Funcion auxiliar que se encarga de pedir tareas y a la vez validarlas
 agregarTareas(Nombre, Puesto, Costo, Rating, Tareas):- write('Ingrese su tarea o la letra s para finalizar: '), 
     read_line_to_string(user_input, TareaNueva), agregarTareasAux(TareaNueva, Tareas, Nombre, Puesto, Costo, Rating).
 
@@ -47,6 +54,7 @@ agregarTareasAux(TareaNueva, Tareas, Nombre, Puesto, Costo, Rating):-
     append(Tareas, [TareaNueva], NuevasTareas), 
     agregarTareas(Nombre, Puesto, Costo, Rating, NuevasTareas).
 
+%Funcion que se encarga de guardar el predicado persona en un txt
 guardar_personas(Nombre, Puesto, Costo, Rating, Tareas):-
     tell('C:/Users/fredd/OneDrive/Documentos/TEC/LENGUAJES/PP3_Fredd_Randall/programa/personas.txt'),
     writeq(persona(Nombre, Puesto, Costo, Rating, Tareas)), write('.\n'),
