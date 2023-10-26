4403997121d9174deeb0aaee5625e57ea3c2d963
Fredd Badilla Víquez <freddalessandro2876@gmail.com>
2023-10-25 22:26:43 -0600

    Update main.pl

--- programa/main.pl
+++ programa/main.pl
@@ -47,25 +47,29 @@ menu:-
     writeln('1. Gestion de personas'),
     writeln('2. Gestion de proyectos'),
     writeln('3. Gestion de tareas'),
+    writeln('6. Asignar tarea'),
+    writeln('7. Cerrar tarea'),
     writeln('9. Salir'),
     write('Ingrese su opcion deseada: '),
-    read_line_to_string(user_input, Opcion), comprobarOpcionMenuMain(Opcion).
+    read_line_to_string(user_input, Opcion), nl, comprobarOpcionMenuMain(Opcion).
 
 %Funcion auxiliar para comprobar que la opcion ingresada por el usuario el correcta
 comprobarOpcionMenuMain(Opcion):- string_lower(Opcion, "1"), menuPersona, menu.
 comprobarOpcionMenuMain(Opcion):- string_lower(Opcion, "2"), menuProyecto, menu.
 comprobarOpcionMenuMain(Opcion):- string_lower(Opcion, "3"), menu_tarea, menu.
+comprobarOpcionMenuMain(Opcion):- string_lower(Opcion, "6"), asignarTarea, menu.
+comprobarOpcionMenuMain(Opcion):- string_lower(Opcion, "7"), cerrarTarea, menu.
 comprobarOpcionMenuMain(Opcion):- string_lower(Opcion, "9").
-comprobarOpcionMenuMain(_):- writeln('Opcion invalida. Por favor, ingrese una opcion valida2.'), menu.
+comprobarOpcionMenuMain(_):- writeln('Opcion invalida. Por favor, ingrese una opcion valida.'), nl, menu.
 
 %Menu para la funcion de gestion de personas
 menuPersona:- writeln('1. Agregar personas'), writeln('2. Imprimir personas'), writeln('3. Volver'), write('Ingrese su opcion: '),
-    read_line_to_string(user_input, Opcion), comprobarOpcionPersona(Opcion).
+    read_line_to_string(user_input, Opcion), nl, comprobarOpcionPersona(Opcion).
 
 %Funcion auxiliar para comprobar que la opcion ingresada por el usuario el correcta
-comprobarOpcionPersona(Opcion):- string_lower(Opcion, "1"), ingresarPersonas, menuPersona.
-comprobarOpcionPersona(Opcion):- string_lower(Opcion, "2"), nl, imprimirPersonas, menuPersona.
-comprobarOpcionPersona(Opcion):- string_lower(Opcion, "3").
+comprobarOpcionPersona(Opcion):- string_lower(Opcion, "1"), ingresarPersonas, !, menuPersona.
+comprobarOpcionPersona(Opcion):- string_lower(Opcion, "2"), nl, imprimirPersonas, !, menuPersona.
+comprobarOpcionPersona(Opcion):- string_lower(Opcion, "3"), !.
 comprobarOpcionPersona(_):- writeln('Opcion invalida. Por favor, ingrese una opcion valida.'), nl, menuPersona.
 
 %Funcion que imprime los hechos de personas
@@ -75,8 +79,17 @@ imprimirPersonas :-
     write('Puesto: '), writeln(Puesto),
     write('Costo: '), writeln(Costo),
     write('Rating: '), writeln(Rating),
+    writeln('\nTareas en la trabaja/o\n'),
+    imprimirPersonasAux(Nombre),
     nl, not(true).
 imprimirPersonas :- true.
+
+imprimirPersonasAux(Nombre):- 
+    tarea(Tarea, Proyecto, _, _, Nombre, _, _),
+    write('Nombre de la tarea: '), writeln(Tarea),
+    write('Nombre del proyecto: '), writeln(Proyecto),
+    nl, not(true).
+
 
 %Funcion que pide los datos al usuario sobre un nuevo empleado
 ingresarPersonas:- write('Ingrese el nombre de su trabajador: '), read_line_to_string(user_input, Nombre), nl,
@@ -139,20 +152,20 @@ guardar_personas(Nombre, Puesto, Costo, Rating, Tareas):-
 
 %Menu para la funcion de gestion de proyectos
 menuProyecto:- writeln('1. Agregar proyectos'), writeln('2. Imprimir proyectos'), writeln('3. Volver'), write('Ingrese su opcion: '),
-    read_line_to_string(user_input, Opcion), comprobarOpcionProyecto(Opcion).
+    read_line_to_string(user_input, Opcion), nl, comprobarOpcionProyecto(Opcion).
 
 %Funcion auxiliar para comprobar que la opcion ingresada por el usuario el correcta
 comprobarOpcionProyecto(Opcion):- string_lower(Opcion, "1"), ingresarProyectos, !, menuProyecto.
 comprobarOpcionProyecto(Opcion):- string_lower(Opcion, "2"), nl, imprimirProyectos, !, menuProyecto.
-comprobarOpcionProyecto(Opcion):- string_lower(Opcion, "3").
+comprobarOpcionProyecto(Opcion):- string_lower(Opcion, "3"), !.
 comprobarOpcionProyecto(_):- writeln('Opcion invalida. Por favor, ingrese una opcion validax.'), nl, menuProyecto.
 
 %Funcion que agrega proyectos junto a sus auxiliares que ayudan a validar
 ingresarProyectos:- write('Ingrese el nombre de su proyecto: '), read_line_to_string(user_input, Nombre), nl,
     write('Ingrese la empresa a la que pertenece este proyecto: '), read_line_to_string(user_input, Empresa), nl,
     write('Ingrese el presupuesto para este proyecto: '), read_line_to_string(user_input, Presupuesto), nl,
-    write('Ingrese una fecha de inicio para este proyecto (Formato DD-MM-YYYY): '), read_line_to_string(user_input, FechaIni), nl,
-    write('Ingrese una fecha de fin para este proyecto (Formato DD-MM-YYYY): '), read_line_to_string(user_input, FechaFin), nl,
+    write('Fecha de inicio para este proyecto (Formato DD-MM-YYYY): '), read_line_to_string(user_input, FechaIni), nl,
+    write('Fecha de fin para este proyecto (Formato DD-MM-YYYY): '), read_line_to_string(user_input, FechaFin), nl,
     ingresarProyectosAux(Nombre, Empresa, Presupuesto, FechaIni, FechaFin).
 
 ingresarProyectosAux(Nombre, _, _, _, _):- proyecto(Nombre, _, _, _, _),
@@ -245,6 +258,24 @@ ingresarProyectosAux(_, _, _, FechaIni, FechaFin):- substring(Cadena1, 0, 2, Fec
     writeln('Formato invalido -> La fecha de inicio no puede ser mayor a la fecha de fin'), nl.
 
 ingresarProyectosAux(_, _, _, FechaIni, FechaFin):- substring(Cadena1, 0, 2, FechaIni), substring(Cadena2, 0, 2, FechaFin),
+    atom_number(Cadena1, Number1), atom_number(Cadena2, Number2), Number1 < Number2,
+    substring(Cadena3, 3, 2, FechaIni), substring(Cadena4, 3, 2, FechaFin),
+    atom_number(Cadena3, Number3), atom_number(Cadena4, Number4), Number3 = Number4,
+    substring(Cadena5, 6, 4,FechaIni), substring(Cadena6, 6, 4,FechaFin),
+    atom_number(Cadena5, Number5), atom_number(Cadena6, Number6),
+    (Number6 = Number5),
+    writeln('Formato invalido -> La fecha de inicio no puede ser mayor a la fecha de fin'), nl.
+
+ingresarProyectosAux(_, _, _, FechaIni, FechaFin):- substring(Cadena1, 0, 2, FechaIni), substring(Cadena2, 0, 2, FechaFin),
+    atom_number(Cadena1, Number1), atom_number(Cadena2, Number2), Number1 = Number2,
+    substring(Cadena3, 3, 2, FechaIni), substring(Cadena4, 3, 2, FechaFin),
+    atom_number(Cadena3, Number3), atom_number(Cadena4, Number4), Number3 = Number4,
+    substring(Cadena5, 6, 4,FechaIni), substring(Cadena6, 6, 4,FechaFin),
+    atom_number(Cadena5, Number5), atom_number(Cadena6, Number6),
+    (Number6 = Number5),
+    writeln('Formato invalido -> La fecha de inicio no puede ser igual a la fecha de fin'), nl.
+
+ingresarProyectosAux(_, _, _, FechaIni, FechaFin):- substring(Cadena1, 0, 2, FechaIni), substring(Cadena2, 0, 2, FechaFin),
     atom_number(Cadena1, Number1), atom_number(Cadena2, Number2), Number1 = Number2,
     substring(Cadena3, 3, 2, FechaIni), substring(Cadena4, 3, 2, FechaFin),
     atom_number(Cadena3, Number3), atom_number(Cadena4, Number4), Number3 >= Number4,
@@ -294,8 +325,8 @@ menu_tarea:-
     read_line_to_string(user_input, Opcion), comprobar_opcion_tarea(Opcion).
 
 %Funcion auxiliar para comprobar que la opcion ingresada por el usuario el correcta
-comprobar_opcion_tarea(Opcion):- string_lower(Opcion, "1"), ingresar_tarea, !, menu_tarea.
-comprobar_opcion_tarea(Opcion):- string_lower(Opcion, "2"), imprimir_tarea, !, menu_tarea.
+comprobar_opcion_tarea(Opcion):- string_lower(Opcion, "1"), ingresar_tarea, nl, !, menu_tarea.
+comprobar_opcion_tarea(Opcion):- string_lower(Opcion, "2"), imprimir_tarea, nl, !, menu_tarea.
 comprobar_opcion_tarea(Opcion):- string_lower(Opcion, "3"), !.
 comprobar_opcion_tarea(_):- writeln('Opcion invalida. Por favor, ingrese una opcion valida.'), menu_tarea.
 
@@ -347,9 +378,9 @@ existe_tarea(Nombre) :-
 validar_fecha(Fecha) :-
     split_string(Fecha, "-", "", Lista),  %hacemos el split por guiones.
     %extraemos las posiciones de la lista.
-    nth0(0, Lista, Year),
+    nth0(2, Lista, Year),
     nth0(1, Lista, Month),
-    nth0(2, Lista, Day),
+    nth0(0, Lista, Day),
     %las transformamos a int.
     str_to_int(Year, Year_int),
     str_to_int(Month, Month_int),
@@ -387,3 +418,135 @@ imprimir_tarea :- true.
 cantidad_proyectos(Cantidad) :-
     findall(_, proyecto(_,_,_,_,_), Proyectos),
     length(Proyectos, Cantidad).
+
+
+%Funcion que se encargar de pedir los datos para asignar una tarea, ademas los valida
+asignarTarea:- write('Ingrese el nombre del proyecto: '), read_line_to_string(user_input, Proyecto), nl,
+    write('Ingrese el nombre de la tarea: '), read_line_to_string(user_input, Tarea), nl,
+    write('Nombre de la persona a asignar: '), read_line_to_string(user_input, Persona), nl,
+    asignarTareaAux(Proyecto, Tarea, Persona).
+
+asignarTareaAux(Proyecto, _, _):- not(proyecto(Proyecto, _, _, _, _)), 
+    writeln('!!Error -> el proyecto ingresado no existe <- Error!!'), nl.
+
+asignarTareaAux(_, Tarea, _):- not(tarea(Tarea, _, _, _, _, _, _)), 
+    writeln('!!Error -> la tarea ingresada no existe <- Error!!'), nl.
+
+asignarTareaAux(_, _, Persona):- not(persona(Persona, _, _, _, _)), 
+    writeln('!!Error -> la persona no existe <- Error!!'), nl.
+
+asignarTareaAux(Proyecto, Tarea, _):- not(tarea(Tarea, Proyecto, _, _, _, _, _)),
+    writeln('!!Error -> la tarea no posee es de ese proyecto <- Error!!'), nl.
+
+asignarTareaAux(Proyecto, Tarea, _):- tarea(Tarea, Proyecto, _, _, Persona, _, _), not(string_lower(Persona, "no asignada")),
+    writeln('!!Error -> la tarea ya tiene encargado <- Error!!'), nl.
+
+asignarTareaAux(Proyecto, Tarea, Persona):- retract(tarea(Tarea, Proyecto, Tipo, _, _, Fecha_inicio, Fecha_final)), 
+    asserta(tarea(Tarea, Proyecto, Tipo, 'activa', Persona, Fecha_inicio, Fecha_final)), writeln('Tarea asignada con exito!!'), nl.
+
+%Funcion que se encargar de pedir los datos para cerrar una tarea, ademas los valida
+cerrarTarea:- write('Ingrese el nombre del proyecto: '), read_line_to_string(user_input, Proyecto), nl,
+    write('Ingrese el nombre de la tarea: '), read_line_to_string(user_input, Tarea), nl,
+    write('Fecha de cierre (Formato DD-MM-YYYY): '), read_line_to_string(user_input, FechaCierre), nl,
+    cerrarTareaAux(Proyecto, Tarea, FechaCierre).
+
+cerrarTareaAux(Proyecto, _, _):- not(proyecto(Proyecto, _, _, _, _)), 
+    writeln('!!Error -> el proyecto ingresado no existe <- Error!!'), nl.
+
+cerrarTareaAux(_, Tarea, _):- not(tarea(Tarea, _, _, _, _, _, _)), 
+    writeln('!!Error -> la tarea ingresada no existe <- Error!!'), nl.
+
+cerrarTareaAux(Proyecto, Tarea, _):- tarea(Tarea, Proyecto, _, _, Persona, _, _), string_lower(Persona, "finalizada"),
+    writeln('!!Error -> la tarea ya ha sido cerrada <- Error!!'), nl.
+
+cerrarTareaAux(_, _, FechaCierre):- not(validar_fecha(FechaCierre)), 
+    writeln('!!Error -> la fecha no sigue el formato especificado <- Error!!'), nl.
+
+cerrarTareaAux(Proyecto, Tarea, FechaCierre):- tarea(Tarea, Proyecto, _, _, _, FechaIni, _), substring(Anio2, 6, 4, FechaIni),
+    substring(Anio1, 6, 4, FechaCierre), atom_number(Anio1, Number1), atom_number(Anio2, Number2),
+    Number2 > Number1,
+    writeln('!!Error -> la fecha de cierre no puede ser menor que la de inicio <- Error!!'), nl.
+
+cerrarTareaAux(Proyecto, Tarea, FechaFin):- tarea(Tarea, Proyecto, _, _, _, FechaIni, _), 
+    substring(Cadena1, 0, 2, FechaIni), substring(Cadena2, 0, 2, FechaFin),
+    atom_number(Cadena1, Number1), atom_number(Cadena2, Number2), Number1 > Number2, 
+    substring(Cadena3, 3, 2, FechaIni), substring(Cadena4, 3, 2, FechaFin),
+    atom_number(Cadena3, Number3), atom_number(Cadena4, Number4), Number3 >= Number4,
+    substring(Cadena5, 6, 4,FechaIni), substring(Cadena6, 6, 4,FechaFin),
+    atom_number(Cadena5, Number5), atom_number(Cadena6, Number6),
+    not(Number6 < Number5),
+    writeln('Formato invalido -> La fecha de inicio no puede ser mayor a la fecha de fin'), nl.
+
+cerrarTareaAux(Proyecto, Tarea, FechaFin):- tarea(Tarea, Proyecto, _, _, _, FechaIni, _), 
+    substring(Cadena1, 0, 2, FechaIni), substring(Cadena2, 0, 2, FechaFin),
+    atom_number(Cadena1, Number1), atom_number(Cadena2, Number2), Number1 < Number2,
+    substring(Cadena3, 3, 2, FechaIni), substring(Cadena4, 3, 2, FechaFin),
+    atom_number(Cadena3, Number3), atom_number(Cadena4, Number4), Number3 > Number4,
+    substring(Cadena5, 6, 4,FechaIni), substring(Cadena6, 6, 4,FechaFin),
+    atom_number(Cadena5, Number5), atom_number(Cadena6, Number6),
+    not(Number6 < Number5),
+    writeln('Formato invalido -> La fecha de inicio no puede ser mayor a la fecha de fin'), nl.
+
+cerrarTareaAux(Proyecto, Tarea, FechaFin):- tarea(Tarea, Proyecto, _, _, _, FechaIni, _), 
+    substring(Cadena1, 0, 2, FechaIni), substring(Cadena2, 0, 2, FechaFin),
+    atom_number(Cadena1, Number1), atom_number(Cadena2, Number2), Number1 = Number2,
+    substring(Cadena3, 3, 2, FechaIni), substring(Cadena4, 3, 2, FechaFin),
+    atom_number(Cadena3, Number3), atom_number(Cadena4, Number4), Number4 =< Number3,
+    substring(Cadena5, 6, 4,FechaIni), substring(Cadena6, 6, 4,FechaFin),
+    atom_number(Cadena5, Number5), atom_number(Cadena6, Number6),
+    not(Number6 < Number5),
+    writeln('Formato invalido -> La fecha de inicio no puede ser mayor o igual a la fecha de fin'), nl.
+
+
+cerrarTareaAux(Proyecto, _, FechaFin):- proyecto(Proyecto, _, _, _, FechaIni), 
+    substring(Cadena1, 0, 2, FechaIni), substring(Cadena2, 0, 2, FechaFin),
+    atom_number(Cadena1, Number1), atom_number(Cadena2, Number2), Number1 > Number2, 
+    substring(Cadena3, 3, 2, FechaIni), substring(Cadena4, 3, 2, FechaFin),
+    atom_number(Cadena3, Number3), atom_number(Cadena4, Number4), Number3 >= Number4,
+    substring(Cadena5, 6, 4,FechaIni), substring(Cadena6, 6, 4,FechaFin),
+    atom_number(Cadena5, Number5), atom_number(Cadena6, Number6),
+    not(Number6 >= Number5),
+    writeln('Formato invalido -> La fecha de fin no puede ser mayor a la fecha de fin del proyecto'), nl.
+
+cerrarTareaAux(Proyecto, _, FechaFin):- proyecto(Proyecto, _, _, _, FechaIni), 
+    substring(Cadena1, 0, 2, FechaIni), substring(Cadena2, 0, 2, FechaFin),
+    atom_number(Cadena1, Number1), atom_number(Cadena2, Number2), Number1 < Number2,
+    substring(Cadena3, 3, 2, FechaIni), substring(Cadena4, 3, 2, FechaFin),
+    atom_number(Cadena3, Number3), atom_number(Cadena4, Number4), Number3 > Number4,
+    substring(Cadena5, 6, 4,FechaIni), substring(Cadena6, 6, 4,FechaFin),
+    atom_number(Cadena5, Number5), atom_number(Cadena6, Number6),
+    not(Number6 >= Number5),
+    writeln('Formato invalido -> La fecha de fin no puede ser mayor a la fecha de fin del proyecto'), nl.
+
+cerrarTareaAux(Proyecto, _, FechaFin):- proyecto(Proyecto, _, _, _, FechaIni), 
+    substring(Cadena1, 0, 2, FechaIni), substring(Cadena2, 0, 2, FechaFin),
+    atom_number(Cadena1, Number1), atom_number(Cadena2, Number2), Number1 < Number2,
+    substring(Cadena3, 3, 2, FechaIni), substring(Cadena4, 3, 2, FechaFin),
+    atom_number(Cadena3, Number3), atom_number(Cadena4, Number4), Number3 = Number4,
+    substring(Cadena5, 6, 4,FechaIni), substring(Cadena6, 6, 4,FechaFin),
+    atom_number(Cadena5, Number5), atom_number(Cadena6, Number6),
+    (Number6 = Number5),
+    writeln('Formato invalido -> La fecha de fin no puede ser mayor a la fecha de fin del proyecto'), nl.
+
+cerrarTareaAux(Proyecto, _, FechaFin):- proyecto(Proyecto, _, _, _, FechaIni), 
+    substring(Cadena1, 0, 2, FechaIni), substring(Cadena2, 0, 2, FechaFin),
+    atom_number(Cadena1, Number1), atom_number(Cadena2, Number2), Number1 = Number2,
+    substring(Cadena3, 3, 2, FechaIni), substring(Cadena4, 3, 2, FechaFin),
+    atom_number(Cadena3, Number3), atom_number(Cadena4, Number4), Number3 = Number4,
+    substring(Cadena5, 6, 4,FechaIni), substring(Cadena6, 6, 4,FechaFin),
+    atom_number(Cadena5, Number5), atom_number(Cadena6, Number6),
+    (Number6 = Number5),
+    writeln('Formato invalido -> La fecha de fin no puede ser igual a la fecha de fin del proyecto'), nl.
+
+cerrarTareaAux(Proyecto, _, FechaIni):- proyecto(Proyecto, _, _, _, FechaIni), 
+    substring(Cadena1, 0, 2, FechaIni), substring(Cadena2, 0, 2, FechaFin),
+    atom_number(Cadena1, Number1), atom_number(Cadena2, Number2), Number1 = Number2,
+    substring(Cadena3, 3, 2, FechaIni), substring(Cadena4, 3, 2, FechaFin),
+    atom_number(Cadena3, Number3), atom_number(Cadena4, Number4), Number3 >= Number4,
+    substring(Cadena5, 6, 4,FechaIni), substring(Cadena6, 6, 4,FechaFin),
+    atom_number(Cadena5, Number5), atom_number(Cadena6, Number6),
+    not(Number6 >= Number5),
+    writeln('Formato invalido -> La fecha de fin no puede ser mayor o igual a la fecha de fin del proyecto'), nl.
+
+cerrarTareaAux(Proyecto, Tarea, FechaFin):- retract(tarea(Tarea, Proyecto, Tipo, _, Persona, Fecha_inicio, _)), 
+    asserta(tarea(Tarea, Proyecto, Tipo, 'finalizada', Persona, Fecha_inicio, FechaFin)), writeln('Tarea cerrada con exito!!'), nl.
